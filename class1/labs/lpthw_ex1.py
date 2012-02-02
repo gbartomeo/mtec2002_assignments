88470effc7fdbcf738f8867d8276cc8dc88076cb
gbartomeo <myrkrlokidottir@hotmail.com>
2012-02-01 19:09:40 -0500

    My first python assignment.

---
+++ class1/labs/lpthw_ex1.py
@@ -0,0 +1,11 @@
+import random
+
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+i = 0
+
+alphlim = random.randint(0,26)
+
+while i < alphlim:
+  print alphabet[i]
+	i = i+1

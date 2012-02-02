5ffe5a53e90ed1d46722627b2958fa720738de2c
gbartomeo <myrkrlokidottir@hotmail.com>
2012-02-02 14:46:01 -0500

    Update class1/labs/lpthw_ex7.py

---
+++ class1/labs/lpthw_ex7.py
@@ -0,0 +1,37 @@
+# Prints "Mary had a little lamb."
+print "Mary had a little lamb."
+# Prints "Its fleece was white as snow."
+print "Its fleece was white as %s." % 'snow'
+# Prints "And everywhere that Mary went."
+print "And everywhere that Mary went."
+# Prints ".........."
+print "." * 10  # Prints 10 of the given string consecutively
+
+# The string variable end1 reads as "C"
+end1 = "C"
+# The string variable end2 reads as "h"
+end2 = "h"
+# The string variable end3 reads as "e"
+end3 = "e"
+# The string variable end4 reads as "e"
+end4 = "e"
+# The string variable end5 reads as "s"
+end5 = "s"
+# The string variable end6 reads as "e"
+end6 = "e"
+# The string variable end7 reads as "B"
+end7 = "B"
+# The string variable end8 reads as "u"
+end8 = "u"
+# The string variable end9 reads as "r"
+end9 = "r"
+# The string variable end10 reads as "g"
+end10 = "g"
+# The string variable end11 reads as "e"
+end11 = "e"
+# The string variable end12 reads as "r"
+end12 = "r"
+
+# Prints, along with the line following it, "Cheese Burger"
+print end1 + end2 + end3 + end4 + end5 + end6, # this comma creates a space and has print continue on this line
+print end7 + end8 + end9 + end10 + end11 + end12

ffadd8b8962304c5146607da34fa30ac80009dd9
gbartomeo <myrkrlokidottir@hotmail.com>
2012-02-01 20:01:33 -0500

    Update class1/labs/lpthw_ex3.py

---
+++ class1/labs/lpthw_ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:" # Chicken countin' time.
+
+print "Hens", 25 + 30 / 6 # PEMDAS. 30 div by 6 = 5, 5+25 = 30!
+print "Roosters", 100 - 25 * 3 % 4 # PEMDAS. 25 times 3 = 75. 75 mod 4 is 3. 100-3 = 97
+
+print "Now I will count the eggs:" # Chickens came before eggs according to Python!
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # PEMDAS. 1/4 = 0. 3+2+1-5+4 = 5. 5%2 = 1. 1-0 = 1. 1+6 = 7
+
+print "Is it true that 3 + 2 < 5 - 7?" # You tell me mister computer.
+
+print 3 + 2 < 5 - 7 # 5 < -2... I think not.
+
+print "What is 3 + 2?", 3 + 2 # 5.
+print "What is 5 - 7?", 5 - 7 # -2
+
+print "Oh, that's why it's False." # No duh.
+
+print "How about some more." # Noooo.
+
+print "Is it greater?", 5 > -2 # Yeah.
+print "Is it greater or equal?", 5 >= -2 # Yep.
+print "Is it less or equal?", 5 <= -2 # Nope.
